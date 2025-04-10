6b6be7c65e96e41436cef233733c72736874dc99
Taylorshmailor <taylorshmailor@gmail.com>
2025-04-10 01:39:56 -0400

    Setting up nav & logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import logo from './assets/vp-logo.webp';
 
 import Home from './components/Home/Home';
+import Work from './components/Work/Work';
 import Resume from './components/Resume/Resume';
+import About from './components/About/About';
 
 function App() {
   return (
     <>
-    <Link to="/">Home</Link>
-    <Link to="/resume">About</Link>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
-    </Routes>
+      <nav className='nav'>
+        <Link to="/">
+          <img src={logo} alt='Logo' className='logo' />
+        </Link>
+        <div className='nav-links'>
+          <Link to="/work">Work</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/about">Me</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Work/Work.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <h2 style={{color: "red"}}>Work</h2>
+  )
+}
+
+export default Work
