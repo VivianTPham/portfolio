c31add84a50d87df590ba16c3d6061a0c13600a8
taylorshmailor <taylorshmailor@gmail.com>
2025-04-17 21:09:57 -0400

    Building about

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import logo from './assets/vp-logo.webp';
 
 import Home from './components/Home/Home';
@@ -9,9 +9,31 @@ import Resume from './components/Resume/Resume';
 import About from './components/About/About';
 
 function App() {
+  const location = useLocation();
+  const [navColor, setNavColor] = useState('');
+
+  useEffect(() => {
+    let color = '#A6BC1B';
+    // Determine the color based on the pathname
+    switch (location.pathname) {
+      case '/about':
+        color = '#A6BC1B';
+        setNavColor('bg-green-500');
+        break;
+      case '/work':
+        color = '#e63a26';
+        setNavColor('bg-red-500');
+        break;
+      default:
+        color = '#e7e4d6';
+        setNavColor('bg-beige-500');
+    }
+    document.body.style.backgroundColor = color;
+  }, [location]);
+
   return (
-    <>
-      <nav className='nav'>
+    <div className={`${navColor}`}>
+      <nav className={`nav ${navColor}`}>
         <Link to="/">
           <img src={logo} alt='Logo' className='logo' />
         </Link>
@@ -27,12 +49,12 @@ function App() {
         <Route path="/resume" element={<Resume />} />
         <Route path="/about" element={<About />} />
       </Routes>
-      <footer className="footer">
+      <footer className={`footer ${navColor}`}>
         <Link to="/">
           <img src={logo} alt='Logo' className='logo' />
         </Link>
       </footer>
-    </>
+    </div>
   );
 }
 
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import profile from '../../assets/placeholder.jpg';
+import galleryImg from '../../assets/placeholder2.jpg';
+
+import './About.css';
 
 const About = () => {
   return (
-    <h2 style={{color: "red"}}>About</h2>
+    <div className="container-about">
+      <div className="about-top">
+        <img src={profile} alt="profile img" className="profile"/>
+        <div>
+          <h2>Hello!</h2>
+          <br />
+          <p>I’m Vivian! I’m an Industrial Designer currently based in Utah.</p>
+          <br />
+          <p>In the past 5 years, I’ve had amazing opportunities to work on various product categories and 
+            collaborating others as a professional industrial designer!</p>
+          <br />
+          <p>For my leisure, I enjoy baking sourdough, ceramics, sewing and hanging out with my dog!</p>
+          <br />
+          <p>For any questions, or interests in working together, contact me at:</p> 
+          <br />
+          <p>vivianpham.id@gmail.com</p>
+        </div>
+      </div>
+      <div className="about-gallery">
+        <img src={galleryImg} alt="Gallery image 1" className="galleryImg"/>
+        <img src={galleryImg} alt="Gallery image 1" className="galleryImg"/>
+        <img src={galleryImg} alt="Gallery image 1" className="galleryImg"/>
+        <img src={galleryImg} alt="Gallery image 1" className="galleryImg"/>
+      </div>
+    </div>
   )
 }
 
