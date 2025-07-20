dcbacf72b6221c6b1906591141fd239302ae11d4
Taylorshmailor <taylorshmailor@gmail.com>
2025-07-20 00:28:14 -0400

    Setting up work pages navigation

---
+++ src/components/Contracting/Contracting.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contracting = () => {
+  return (
+    <>
+      <div style={{color: "red"}}>Contracting</div>
+      <a style={{color: "red"}} href="#/blackstone">BLACKSTONE {'>'}</a>
+    </>
+  )
+}
+
+export default Contracting
